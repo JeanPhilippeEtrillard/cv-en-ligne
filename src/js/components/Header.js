109bed5e6a1545b8c2e8683bd5c07d331034ba75
Filou911 <jphetr@gmail.com>
2017-05-31 18:40:57 +0200

    correction header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,15 +2,14 @@
  * Created by jphet on 15/03/2017.
  */
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class Header extends React.Component {
     render() {
         return <header>
             <nav>
                 <div class="nav-wrapper light-blue darken-2">
-                    <NavLink to="/" activeClassName="brand-logo">Logo</NavLink>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <ul id="nav-mobile" class="right">
                         <li><Link to="/">Accueil</Link></li>
                         <li><Link to="/cv">CV</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
