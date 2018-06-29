8e2fb06a3f1269c33998f5fca8337bd1756ac23c
Jean-Philippe ETRILLARD <jphetr@gmail.com>
2018-06-29 23:54:00 +0200

    Add print fonction

--- src/js/components/Star.js
+++ src/js/components/Star.js
@@ -4,29 +4,40 @@
 import React from "react";
 
 export default class Star extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    isStarEnable(degCon, ref) {
-        return (degCon >= ref ? "starEnable" : "starDisable");
-    }
+  isStarEnable(degCon, ref) {
+    return degCon >= ref ? "starEnable" : "starDisable";
+  }
 
-    render() {
-        const {degCon} = this.props;
+  render() {
+    const { degCon } = this.props;
 
-        const star1 = "fa fa-1 fa-star " + this.isStarEnable(degCon, 1);
-        const star2 = "fa fa-1 fa-star " + this.isStarEnable(degCon, 2);
-        const star3 = "fa fa-1 fa-star " + this.isStarEnable(degCon, 3);
-        const star4 = "fa fa-1 fa-star " + this.isStarEnable(degCon, 4);
-        const star5 = "fa fa-1 fa-star " + this.isStarEnable(degCon, 5);
+    const star1 = "fas fa-star no-print " + this.isStarEnable(degCon, 1);
+    const star2 = "fas fa-star no-print " + this.isStarEnable(degCon, 2);
+    const star3 = "fas fa-star no-print " + this.isStarEnable(degCon, 3);
+    const star4 = "fas fa-star no-print " + this.isStarEnable(degCon, 4);
+    const star5 = "fas fa-star no-print " + this.isStarEnable(degCon, 5);
 
-        return <div className="star">
-            <i className={star1}></i>
-            <i className={star2}></i>
-            <i className={star3}></i>
-            <i className={star4}></i>
-            <i className={star5}></i>
-        </div>
-    }
+    return (
+      <div className="star">
+        <span class="fas fa-stack starEnable hidden">
+          <i class="fas fa-star fa-stack-2x" />
+          <span class="fas fa-stack-1x textStar">
+            <span>
+            {degCon}
+            </span>
+          </span>
+        </span>
+
+        <i className={star1} />
+        <i className={star2} />
+        <i className={star3} />
+        <i className={star4} />
+        <i className={star5} />
+      </div>
+    );
+  }
 }
